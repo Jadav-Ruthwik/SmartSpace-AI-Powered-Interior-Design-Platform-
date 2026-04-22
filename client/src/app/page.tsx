cde362af28ffa59340629abeb6d82d37e951e04a
Jadav-Ruthwik <23211a05b9@bvrit.ac.in>
2026-04-23 03:41:45 +0530

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,28 +1,3 @@
-"use client";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Generator from "../components/Generator";
-import Features from "../components/Features";
-import HowItWorks from "../components/HowItWorks";
-import Showcase from "../components/Showcase";
-import Testimonials from "../components/Testimonials";
-import FAQ from "../components/FAQ";
-import CTA from "../components/CTA";
-import Footer from "../components/Footer";
-
 export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Generator />
-      <Features />
-      <HowItWorks />
-      <Showcase />
-      <Testimonials />
-      <FAQ />
-      <CTA />
-      <Footer />
-    </main>
-  );
+  return <h1>Working ✅</h1>;
 }
